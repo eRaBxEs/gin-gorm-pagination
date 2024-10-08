86697e32d9976a78f6bbd67277f92074ff78ce58
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-08 12:22:17 +0100

    add connection to database

---
+++ before/models/setup.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(mysql.Open("henry_dev:devdba_user@tcp(127.0.0.1:3307)/gorm_testdb?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	DB = database
+}
+
+func DBMigrate() {
+	DB.AutoMigrate(&Blog{})
+}
