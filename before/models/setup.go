47cf520db97d3a698982baa056b985b729359061
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-09 22:58:24 +0100

    change connection string

--- before/models/setup.go
+++ before/models/setup.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(mysql.Open("henry_dev:devdba_user@tcp(127.0.0.1:3307)/gorm_testdb?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	database, err := gorm.Open(mysql.Open("root:devdba_sys@tcp(127.0.0.1:3307)/gin_blogs?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
