a654777febcd0cdf1f13a5e762cd7a79a3a416bf
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-10 23:49:40 +0100

    chaining scopes for use in our model for pagination

--- after/models/blog.go
+++ after/models/blog.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -12,9 +13,9 @@ type Blog struct {
 	Content string `gorm:"type:text"`
 }
 
-func BlogsAll() *[]Blog {
+func BlogsAll(ctx *gin.Context) *[]Blog {
 	var blogs []Blog
-	DB.Where("deleted_at is NULL").Order("updated_at desc").Find(&blogs)
+	DB.Where("deleted_at is NULL").Scopes(Paginate(ctx)).Order("updated_at desc").Find(&blogs)
 	fmt.Println(blogs)
 	return &blogs
 }
